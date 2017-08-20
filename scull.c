376596648deefa2b4a2232832ce5da6b1addaa00
LT <lighttime0@gmail.com>
2017-08-20 10:52:29 +0800

    Version-0.3 : test read and write

--- scull.c
+++ scull.c
@@ -3,19 +3,76 @@
 #include <linux/moduleparam.h>
 
 #include <linux/kernel.h>		/* printk() */
-#include <linux/types.h>		/* Data types */
-#include <linux/kdev_t.h>		/* MKDEV */
+#include <linux/types.h>		/* Data types, such as dev_t */
+#include <linux/kdev_t.h>		/* MKDEV, MAJOR, MINOR */
 #include <linux/fs.h>			/* Register dev */
 
 int scull_major = 0;
 int scull_minor = 0;
-int scull_nr_devs = 1;			/* Number of vare scull devices */
+int scull_nr_devs = 1;			/* Number of bare scull devices */
+
+char *scull_data = NULL;
+
+ssize_t scull_read(struct file *file_p, char __user *buff, size_t count, loff_t *f_ops)
+{
+	int device_data_length = strlen(scull_data);
+
+	if (*f_ops >= device_data_length)
+		return 0;
+
+	if ((count + *f_ops) > device_data_length)
+		count = device_data_length;
+
+	/* function to copy kernel space buffer to user space */
+	if (copy_to_user(buff, scull_data, count) != 0) {
+		printk(KERN_ALERT "Kernel Space to User Space copy failure");
+		return -EFAULT;
+	}
+
+	/* Increment the offset to the number of bytes read */
+	*f_ops += count;
+
+	/* Return the number of bytes copied to the user space */
+	return count;	
+}
+
+ssize_t helloworld_driver_write(struct file * file_p, const char *buff, size_t count, loff_t * f_ops)
+{
+	/* Free the previouosly stored data */
+	if (scull_data)
+		kfree(scull_data);
+
+	/* Allocate new memory for holding the new data */
+	scull_data = kmalloc((count * (sizeof(char *))), GFP_KERNEL);
+
+	/* function to copy user space buffer to kernel space */
+	if (copy_from_user(scull_data, buff, count) != 0) {
+		printk(KERN_ALERT "User Space to Kernel Space copy failure");
+		return -EFAULT;
+	}
+
+	/* Since our device is an array, we need to do this to terminate the string.
+	 * Last character will get overwritten by a \0. Incase of an actual file,
+	 * we will probably update the file size, IIUC.
+	 */
+	scull_data [count] = '\0';
+
+	/* Return the number of bytes actually written. */
+	return count;
+}
+
+//Connect system call(open,read,etc.) to functions above.
+struct file_operations scull_fops = {
+	.owner = THIS_MODULE,		//THIS_MODULE defined in <linux/module.h>
+	.read = scull_read,
+	.write = scull_write,
+}
 
 void scull_clean_module(void)
 {
 	dev_t dev = MKDEV(scull_major, scull_minor);
 
-	if (scull_major)		//If scull_major doesn't equal 0, means scull allocated successfully
+	if (scull_major)		//If scull_major doesn't equal 0, means scull alloc_chrdev_region successfully
 	{	
 		unregister_chrdev_region(dev, scull_nr_devs);
 	}
